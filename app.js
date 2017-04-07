17a81ef0ae0d5dc19c36a0304a518740c86cc3e1
Daniel Esmail <danesmail29@gmail.com>
2017-04-07 02:35:03 -0500

    added a working forward button

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 var players =["Mahi", "Rachel", "Alexis","Kalyee","Kushi","Caitlyn","Julianna","Kushali","Hannah"];
+var poistion = 2;
+
 
 function playerReady(arr){
   for(var i = 0; i<arr.length; i++){
     $("#spot" + (i+1)).html(arr[i])
   }
+}
+
+function forward(arr){
+  if(poistion >= 10 ){
+    poistion = 1;
+  }
+  else{
+
+  }
+  var num = poistion;
+  for(var i = 0; i < arr.length; i++){
+    if(num < 9){
+      $("#spot" + num).html(arr[i])
+      console.log("#spot" + num)
+      console.log(arr[i])
+      num += 1
+
+    }
+    else{
+      $("#spot" + num).html(arr[i])
+      num = 1
+      console.log("#spot" + num)
+      console.log("over 9")
+    }
+  }
+  if(poistion <= 10 ){
+    poistion += 1;
+  }
+  else{
+    poistion = 1;
+  }
+  console.log(poistion + "-----")
 }
 
 
@@ -12,8 +46,9 @@ function playerReady(arr){
 
 $(document).ready(function(){
   playerReady(players)
-  $("#switch").click(function(){
+  $("#forward").click(function(){
     $("#title").html("it worked")
+    forward(players)
 
   })
 })
