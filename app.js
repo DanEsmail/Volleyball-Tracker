487e11f5c49c34411d11d7028c875fd6d6187d18
Daniel Esmail <danesmail29@gmail.com>
2017-04-07 04:47:11 -0500

    added reverse button and fixed forward button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var players =["Mahi", "Rachel", "Alexis","Kalyee","Kushi","Caitlyn","Julianna","Kushali","Hannah"];
-var poistion = 2;
-
+var poistion = 1;
+var testArr =[1,2,3,4,5,6,7,8,9];
 
 function playerReady(arr){
   for(var i = 0; i<arr.length; i++){
@@ -9,46 +9,64 @@ function playerReady(arr){
 }
 
 function forward(arr){
-  if(poistion >= 10 ){
+  if(poistion === 9 ){
     poistion = 1;
   }
   else{
-
+    poistion +=1;
   }
   var num = poistion;
-  for(var i = 0; i < arr.length; i++){
-    if(num < 9){
-      $("#spot" + num).html(arr[i])
-      console.log("#spot" + num)
-      console.log(arr[i])
-      num += 1
+  console.log(num)
+  for (var i = 0; i < arr.length; i++) {
+    if(num <= 9 ){
+    $("#spot" + num).html(arr[i])
+    num += 1
+  }else{
+    num = 1
+    $("#spot" + num).html(arr[i])
+    num += 1
+  }
+}
+}
 
-    }
-    else{
+function reverse(arr){
+  if(poistion <= 1){
+    poistion = 9
+  }else{
+    poistion -= 1
+  }
+  var num = poistion
+  console.log(num)
+    for (var i = 0; i < arr.length; i++) {
+      if(num <= 9 ){
       $("#spot" + num).html(arr[i])
+      num += 1
+    }else{
       num = 1
-      console.log("#spot" + num)
-      console.log("over 9")
+      $("#spot" + num).html(arr[i])
+      num += 1
     }
   }
-  if(poistion <= 10 ){
-    poistion += 1;
-  }
-  else{
-    poistion = 1;
-  }
-  console.log(poistion + "-----")
+
+
+
+
+
 }
 
 
 
 
 
-$(document).ready(function(){
-  playerReady(players)
-  $("#forward").click(function(){
-    $("#title").html("it worked")
-    forward(players)
 
+$(document).ready(function(){
+  playerReady(players);
+  $("#reverse").click(function(){
+    reverse(players)
   })
+  $("#forward").click(function(){
+    forward(players)
+  })
+
+
 })
