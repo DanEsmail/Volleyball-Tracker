b623e550f19a248434985a573ff4563f7e6279a0
Daniel Esmail <danesmail29@gmail.com>
2017-04-18 06:39:18 -0500

    add a select option for amount of players

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var players =["Mahi", "Rachel", "Alexis","Kalyee","Kushi","Caitlyn","Julianna","Kushali","Hannah"];
+var players =[ "Rachel","Alexis","Kushi",  "Caityln",  "Hannah", "Kushali",  "Kaylee", "Mahi",];
 var poistion = 1;
 var testArr =[1,2,3,4,5,6,7,8,9];
+var amount = 6;
 
 function playerReady(arr){
   for(var i = 0; i<arr.length; i++){
@@ -12,7 +13,7 @@ function loop(arr){
     var num = poistion;
     console.log(num)
     for (var i = 0; i < arr.length; i++) {
-      if(num <= 9 ){
+      if(num <= amount ){
       $("#spot" + num).html(arr[i])
       num += 1
     }else{
@@ -25,7 +26,7 @@ function loop(arr){
 
 
 function forward(arr){
-  if(poistion === 9 ){
+  if(poistion == amount ){
     poistion = 1;
   }
   else{
@@ -36,25 +37,77 @@ function forward(arr){
 
 function reverse(arr){
   if(poistion <= 1){
-    poistion = 9
+    poistion = amount
   }else{
     poistion -= 1
   }
   loop(arr)
 }
 
-
+function displayOut(num){
+  switch (num) {
+    case "6":
+    //hide
+      $("#spot7").css("display", "none");
+      $("#spot8").css("display", "none");
+      $("#spot9").css("display", "none");
+      $("#spot10").css("display", "none");
+      break;
+    case "7":
+      //hide
+      $("#spot8").css("display", "none");
+      $("#spot9").css("display", "none");
+      $("#spot10").css("display", "none");
+      //show
+      $("#spot7").css("display", "");
+      break;
+    case "8":
+      //hide
+      $("#spot9").css("display", "none");
+      $("#spot10").css("display", "none");
+      //show
+      $("#spot7").css("display", "");
+      $("#spot8").css("display", "");
+      break;
+    case "9":
+      //hide
+      $("#spot10").css("display", "none");
+      //show
+      $("#spot7").css("display", "");
+      $("#spot8").css("display", "");
+      $("#spot9").css("display", "");
+      break;
+    case "10":
+      //show
+      $("#spot7").css("display", "");
+      $("#spot8").css("display", "");
+      $("#spot9").css("display", "");
+      $("#spot10").css("display", "");
+      break;
+    default:
+      console.log("its a string not a number")
+  }
+}
 
 
 
 
 $(document).ready(function(){
+  amount = $("#amount").val()
+  displayOut($("#amount").val())
   playerReady(players);
   $("#reverse").click(function(){
     reverse(players)
   })
   $("#forward").click(function(){
     forward(players)
+  })
+
+  $("#amount").change(function(){
+    amount = $("#amount").val()
+    displayOut($("#amount").val())
+    $("#title").html($("#amount").val())
+
   })
 
 
